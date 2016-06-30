b57168931309d5cf4128d99705eb160f0f0a86dc
alberto56 <albert@mediatribe.net>
2016-06-29 21:41:47 -0400

    Issue #5 warn if git submodule not installed

--- tests/end-to-end-test.sh
+++ tests/end-to-end-test.sh
@@ -2,5 +2,5 @@ set -e
 
 ./dcycle-metrics-engine.sh metrics --source=tests/sample-code/sample-001 --destination=tmp/test-metrics
 
-cat tmp/test-metrics/metrics/raw/pdepend/summary-xml.xml |grep lloc
-cat tmp/test-metrics/metrics/raw/phpcpd/phpcpd.xml |grep duplication
+grep lloc < ./tmp/test-metrics/metrics/raw/pdepend/summary-xml.xml
+grep duplication < ./tmp/test-metrics/metrics/raw/phpcpd/phpcpd.xml
